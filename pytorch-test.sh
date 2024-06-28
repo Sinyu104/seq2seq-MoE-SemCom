d70a958de8b9ef045cd2fbb48cd6c56a623da41d
Sinyu104 <brandyhuang104@gmail.com>
2024-06-28 13:10:00 -0700

    fix bugs on run on CC cedar

--- pytorch-test.sh
+++ pytorch-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --gres=gpu:v100l:1       # Request GPU "generic resources"
+#SBATCH --gres=gpu:p100:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --mem=16000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=2-00:00
 #SBATCH --output=%N-%j.out
 
