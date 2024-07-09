3a623b38d7d3af6f7591faccc106a4baed644747
Sinyu104 <brandyhuang104@gmail.com>
2024-07-08 21:57:35 -0700

    reduce batch size

--- utils.py
+++ utils.py
@@ -193,7 +193,7 @@ class NativeScaler:
         self.scaler = GradScaler(init_scale=scale_factor)
         self.max_norm = max_norm
 
-    def __call__(self, step, loss, optimizer, accumulation_steps=1):
+    def __call__(self, step, loss, optimizer, accumulation_steps=3):
         loss = loss / accumulation_steps # Scale loss
         self.scaler.scale(loss).backward() # Compute gradients
         
